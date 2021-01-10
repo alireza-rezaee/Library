4d813f396092077ab3c8c134fe55a76478944a93
Alireza Rezaee <alireza-git-commits@rezaee.org>
2021-01-11 02:05:32 +0330

    Add TagHelpers
    - GravatarTagHelper
    - ToastsTagHelper

---
+++ src/Library.WebApp/Extensions/SessionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Mohkazv.Library.WebApp.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Mohkazv.Library.WebApp.Extensions
+{
+    public static class SessionExtensions
+    {
+        public static void SetObjectAsJson(this ISession session, string key, object value) => session.SetString(key, JsonSerializer.Serialize(value));
+
+        public static T GetObjectFromJson<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+
+            return value == null ? default : JsonSerializer.Deserialize<T>(value);
+        }
+
+        public static IList<ToastViewModel> GetToasts(this ISession session)
+        {
+            var toasts = new List<ToastViewModel>();
+            foreach (var toastkey in ToastKeys(session.Keys))
+            {
+                toasts.Add(session.GetObjectFromJson<ToastViewModel>(toastkey));
+                session.Remove(toastkey);
+            }
+            return toasts;
+        }
+
+        public static void SetToast(this ISession session, ToastViewModel toast) => session.SetObjectAsJson($"toast-{Guid.NewGuid()}", toast);
+
+        private static bool HasToastSigniture(string key) => key.StartsWith("toast-");
+
+        private static string[] ToastKeys(IEnumerable<string> keys) => keys.Where(key => HasToastSigniture(key)).ToArray();
+    }
+}
---
+++ src/Library.WebApp/Models/ViewModels/ToastViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Mohkazv.Library.WebApp.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mohkazv.Library.WebApp.Models.ViewModels
+{
+    public class ToastViewModel
+    {
+        public ToastType? Type { get; set; }
+
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime? Time { get; set; }
+
+        public bool? Animation { get; set; }
+
+        public bool? Autohide { get; set; }
+
+        public int? Delay { get; set; }
+    }
+}
