dfd54b522e545d0e0bf1bcf510963694d021bb7c
Alireza Rezaee <alireza-git-commits@rezaee.org>
2021-01-10 11:41:15 +0330

    Update BorrowBook model

--- src/Library.WebApp/Models/BorrowBook.cs
+++ src/Library.WebApp/Models/BorrowBook.cs
@@ -1,6 +1,7 @@
 ﻿using Mohkazv.Library.WebApp.Areas.Identity.Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,15 @@ namespace Mohkazv.Library.WebApp.Models
         public int BookId { get; set; }
 
         public string UserId { get; set; }
+
+        [Display(Name = "زمان تحویل از کتابخانه")]
+        public DateTime? DeliveryDateTime { get; set; }
+
+        [Display(Name = "موعد پس‌دادن به کتابخانه")]
+        public DateTime? ReturnDateTime { get; set; }
+
+        [Display(Name = "زمان پس‌دادن به کتابخانه")]
+        public DateTime? ReturnedDateTime { get; set; }
 
         #region Relationships
         public Book Book { get; set; }
