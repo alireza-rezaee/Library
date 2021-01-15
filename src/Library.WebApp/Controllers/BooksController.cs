c9d2479cb41803cd29c33b67dab252bcd6f37415
Alireza Rezaee <alireza-git-commits@rezaee.org>
2021-01-15 12:20:06 +0330

    add book image feature

--- src/Library.WebApp/Controllers/BooksController.cs
+++ src/Library.WebApp/Controllers/BooksController.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Mohkazv.Library.WebApp.Data;
+using Mohkazv.Library.WebApp.Extensions;
+using Mohkazv.Library.WebApp.Helpers;
 using Mohkazv.Library.WebApp.Models;
+using Mohkazv.Library.WebApp.Models.ViewModels.Books;
 
 namespace Mohkazv.Library.WebApp.Controllers
 {
@@ -14,8 +19,11 @@ namespace Mohkazv.Library.WebApp.Controllers
     public class BooksController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IFileManager _ifileManager;
+        private readonly IWebHostEnvironment _env;
 
-        public BooksController(ApplicationDbContext context) => _context = context;
+        public BooksController(ApplicationDbContext context, IFileManager ifileManager, IWebHostEnvironment env)
+            => (_context, _ifileManager, _env) = (context, ifileManager, env);
 
         [HttpGet("")]
         public async Task<IActionResult> Index()
@@ -52,21 +60,37 @@ namespace Mohkazv.Library.WebApp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost("create")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Isbn,Name,Description,CoverPath,PublishInformation,TypeId,LanguageId,PublisherId,DdcId")] Book book)
+        public async Task<IActionResult> Create(CreateEditViewModel vm)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(book);
+                if (vm.BookImage != null)
+                {
+                    var persianDateTime = PersianDateTime.Now;
+                    var dateTime = persianDateTime.ToDateTime();
+
+                    // 2MiB = 2097152 bytes
+                    vm.BookImage.Check(2097152, new string[] { "image/jpg", "image/jpeg", "image/png", "image/gif" });
+
+                    var imagePath = $"uploads/images/{persianDateTime.ToString("yyyy/MM/dd/yyyyMMddhhmmss") + DateTime.Now.ToString("ffff") + new Random().Next(1000000, 9999999)}_{Helpers.File.ValidateName(vm.BookImage.FileName)}";
+
+                    await _ifileManager.SaveFile(vm.BookImage, imagePath);
+
+                    vm.Book.CoverPath = $"/{imagePath}";
+                }
+
+                _context.Add(vm.Book);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["DdcId"] = new SelectList(_context.DeweyDecimalClassifications, "Id", "Id", book.DdcId);
-            ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Name", book.LanguageId);
-            ViewData["PublisherId"] = new SelectList(_context.Publishers, "Id", "Title", book.PublisherId);
-            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Title", book.TypeId);
-            return View(book);
+            ViewData["DdcId"] = new SelectList(_context.DeweyDecimalClassifications, "Id", "Id", vm.Book.DdcId);
+            ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Name", vm.Book.LanguageId);
+            ViewData["PublisherId"] = new SelectList(_context.Publishers, "Id", "Title", vm.Book.PublisherId);
+            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Title", vm.Book.TypeId);
+            return View(vm);
         }
 
+        [HttpGet("edit/{id}")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -76,37 +100,63 @@ namespace Mohkazv.Library.WebApp.Controllers
 
             var book = await _context.Books.FindAsync(id);
             if (book == null)
-            {
                 return NotFound();
-            }
+
             ViewData["DdcId"] = new SelectList(_context.DeweyDecimalClassifications, "Id", "Id", book.DdcId);
             ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Name", book.LanguageId);
             ViewData["PublisherId"] = new SelectList(_context.Publishers, "Id", "Title", book.PublisherId);
             ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Title", book.TypeId);
-            return View(book);
+
+            return View(new CreateEditViewModel { Book = book });
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost("edit/{id}")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Isbn,Name,Description,CoverPath,PublishInformation,TypeId,LanguageId,PublisherId,DdcId")] Book book)
+        public async Task<IActionResult> Edit(int? id, CreateEditViewModel vm)
         {
-            if (id != book.Id)
-            {
+            if (id == null)
                 return NotFound();
-            }
+
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+                return NotFound();
 
             if (ModelState.IsValid)
             {
                 try
                 {
+                    book.Name = vm.Book.Name;
+                    book.Isbn = vm.Book.Isbn;
+                    book.Description = vm.Book.Description;
+                    book.PublishInformation = vm.Book.PublishInformation;
+                    book.TypeId = vm.Book.TypeId;
+                    book.DdcId = vm.Book.DdcId;
+                    book.PublisherId = vm.Book.PublisherId;
+                    book.LanguageId = vm.Book.LanguageId;
+
+                    if (vm.BookImage != null)
+                    {
+                        var persianDateTime = PersianDateTime.Now;
+                        var dateTime = persianDateTime.ToDateTime();
+
+                        // 2MiB = 2097152 bytes
+                        vm.BookImage.Check(2097152, new string[] { "image/jpg", "image/jpeg", "image/png", "image/gif" });
+
+                        var imagePath = $"uploads/images/{persianDateTime.ToString("yyyy/MM/dd/yyyyMMddhhmmss") + DateTime.Now.ToString("ffff") + new Random().Next(1000000, 9999999)}_{Helpers.File.ValidateName(vm.BookImage.FileName)}";
+
+                        await _ifileManager.SaveFile(vm.BookImage, imagePath);
+
+                        book.CoverPath = $"/{imagePath}";
+                    }
+
                     _context.Update(book);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!BookExists(book.Id))
+                    if (!BookExists(vm.Book.Id))
                     {
                         return NotFound();
                     }
@@ -117,11 +167,11 @@ namespace Mohkazv.Library.WebApp.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["DdcId"] = new SelectList(_context.DeweyDecimalClassifications, "Id", "Id", book.DdcId);
-            ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Name", book.LanguageId);
-            ViewData["PublisherId"] = new SelectList(_context.Publishers, "Id", "Title", book.PublisherId);
-            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Title", book.TypeId);
-            return View(book);
+            ViewData["DdcId"] = new SelectList(_context.DeweyDecimalClassifications, "Id", "Id", vm.Book.DdcId);
+            ViewData["LanguageId"] = new SelectList(_context.Languages, "Id", "Name", vm.Book.LanguageId);
+            ViewData["PublisherId"] = new SelectList(_context.Publishers, "Id", "Title", vm.Book.PublisherId);
+            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Title", vm.Book.TypeId);
+            return View(vm);
         }
 
         [HttpGet("delete/{id}")]
@@ -151,6 +201,9 @@ namespace Mohkazv.Library.WebApp.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var book = await _context.Books.FindAsync(id);
+
+            _ifileManager.DeleteFile(book.CoverPath);
+
             _context.Books.Remove(book);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
---
+++ src/Library.WebApp/Models/ViewModels/Books/CreateEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mohkazv.Library.WebApp.Models.ViewModels.Books
+{
+    public class CreateEditViewModel
+    {
+        public Book Book { get; set; }
+
+        public IFormFile BookImage { get; set; }
+    }
+}
